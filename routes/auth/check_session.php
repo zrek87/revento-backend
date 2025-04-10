a7bfb3e6e3c2e5a9bb95f7dc75da4443deadaaae
zrek87 <93806801+zrek87@users.noreply.github.com>
2025-04-11 00:09:47 +0300

    Update check_session.php

--- routes/auth/check_session.php
+++ routes/auth/check_session.php
@@ -5,9 +5,11 @@ ini_set('display_errors', 1);
 include('../../includes/session.php');
 include('../../includes/functions.php');
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://ckkso0s04080wkgskwkowwso.217.65.145.182.sslip.io");
 header("Access-Control-Allow-Credentials: true");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
