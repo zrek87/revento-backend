934f5bf018641ac7522e73ea0d5941d7aa0a20f9
zrek87 <93806801+zrek87@users.noreply.github.com>
2025-04-11 00:10:29 +0300

    Update signin.php

--- routes/auth/signin.php
+++ routes/auth/signin.php
@@ -3,11 +3,11 @@ include('../../includes/session.php');
 include('../../includes/conn.php');
 include('../../includes/functions.php');
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Origin: http://ckkso0s04080wkgskwkowwso.217.65.145.182.sslip.io");
 header("Access-Control-Allow-Credentials: true");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
