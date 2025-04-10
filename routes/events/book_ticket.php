6615e0f85f437fa036521a66a15adf15250033dc
zrek87 <93806801+zrek87@users.noreply.github.com>
2025-04-11 00:11:09 +0300

    Update book_ticket.php

--- routes/events/book_ticket.php
+++ routes/events/book_ticket.php
@@ -4,11 +4,11 @@ ini_set('display_errors', 1);
 
 session_start();
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Origin: http://ckkso0s04080wkgskwkowwso.217.65.145.182.sslip.io");
 header("Access-Control-Allow-Credentials: true");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
 
 require_once "../../includes/conn.php";
 
